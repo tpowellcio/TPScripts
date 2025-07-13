c119e636adc343892f4e5006f8f630460bc943b9
Troy Powell <troypowell28@gmail.com>
2025-07-12 18:22:07 -0800

    Adding thumbnail conversion to jpg

--- fishScripts/mp3p.fish
+++ fishScripts/mp3p.fish
@@ -3,6 +3,8 @@ function mp3p
     yt-dlp $argv \
         --ignore-errors \
         --embed-thumbnail \
+        --convert-thumbnails jpg \
+        --ppa "ffmpeg_o:ThumbnailsConvertor:-q:v 1" \
         --extract-audio \
         --audio-format mp3 \
         --audio-quality 0 \
